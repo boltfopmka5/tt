62c3088e0b5d7051a5878b1f796c4ef56e38c2a9
m5stacker <boltfop101911@gmail.com>
2026-05-29 14:05:38 +0300

    Delete stop_all.sh

--- stop_all.sh
+++
@@ -1,9 +0,0 @@
-cat > /opt/bot/stop_all.sh << 'EOF'
-#!/bin/bash
-echo "=== Stopping all bots ==="
-systemctl stop tradeall-bot
-systemctl stop tradeall-autoreply
-echo "Done. All bots stopped."
-EOF
-
-chmod +x /opt/bot/stop_all.sh
